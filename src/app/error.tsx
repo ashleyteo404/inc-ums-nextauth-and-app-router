efcc5648147ca143cf8d994369012ec752d077f7
ashleyteo404 <ashleyteo404@gmail.com>
2024-02-10 21:23:46 +0800

    error page for landing page

---
+++ src/app/error.tsx
@@ -0,0 +1,26 @@
+'use client' // Error components must be Client Components
+ 
+import Link from 'next/link'
+import { Button } from '~/components/ui/button'
+ 
+export default function Error({
+  error,
+}: {
+  error: Error & { digest?: string }
+}) { 
+  return (
+    <div> 
+        <div className="flex flex-col items-center justify-center">
+            <h2 className="flex text-4xl">Something went wrong!</h2>
+            <p className="flex m-4 text-2xl font-bold text-red-600">
+                {error.message}
+            </p>
+            <Link href={`/`} className="flex items-center">
+                <Button variant={"default"}>
+                    Go Home
+                </Button>
+            </Link>
+        </div>
+    </div>
+  )
+}
